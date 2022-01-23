470f96779e2e4680709b362147f3571fc8244324
hyeonjin25 <shj41738866@gmail.com>
2022-01-23 17:28:21 +0900

    add textQuery function

--- client/src/chatbot/ChatbotScreen.js
+++ client/src/chatbot/ChatbotScreen.js
@@ -1,6 +1,48 @@
+import Axios from "axios";
 import React from "react";
 
 function ChatbotScreen() {
+  const textQuery = async (text) => {
+    // 1. 사용자가 입력한 메세지 처리
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text: text,
+        },
+      },
+    };
+
+    const textQueryVariables = {
+      text,
+    };
+    // 2. 챗봇이 보낸 메세지 처리
+    try {
+      // textQuery Rout에 메세지 보내기
+      const response = await Axios.post(
+        "api/dialogflow/textQuery",
+        textQueryVariables
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content: content,
+      };
+      console.log(conversation);
+    } catch (err) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: " Error just occured, please check the problem",
+          },
+        },
+      };
+      console.log(conversation);
+    }
+  };
+
   const KeyPressHandler = (e) => {
     if (e.key == "Enter") {
       if (!e.target.value) {
@@ -8,6 +50,7 @@ function ChatbotScreen() {
         return alert("you need to type something first");
       }
       // 사용자가 입력한 값을 서버로 보내기
+      textQuery(e.target.value);
 
       e.target.value = "";
     }
